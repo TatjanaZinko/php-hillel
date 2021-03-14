4b53285cb7fcb8e1ad91cf603a9aee284bcf1211
Zinko <ztvproweb@gmail.com>
2021-03-14 12:38:13 +0200

    Lesson 3, 1 edition

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 /*
  * First Task
  */
-$arr = [5,7,4,[2,[2,3,2],3,1],8,[1,4],3];
+//$arr = [5,7,4,[2,[2,3,2],3,1],8,[1,4],3];
+$arr = [5,'text',4,[2,[2,3,2],3,1],8,[1,4],3];
 $sum = 0;
 function sum_second_element($arr, &$sum){
-        if (!is_array($arr[1])) {
-            $sum += $arr[1];
+        $second_elem = next($arr);
+        if (!is_array($second_elem)) {
+            $num = (float) $second_elem;
+            $sum += $num;
         }
     foreach ($arr as $key=>$value) {
         if (is_array($value)) {
@@ -17,11 +20,12 @@ function sum_second_element($arr, &$sum){
 sum_second_element($arr, $sum);
 echo 'Сумма всех вторых элементов равна: ' . $sum;
 echo '<br>';
+
 /*
  * Second Task
  */
-$string = 'abhvava';
-$arr = str_split($string);
+$string = 'агава';
+$arr = preg_split('//u',$string,-1,PREG_SPLIT_NO_EMPTY);
 $result = array_count_values($arr);
 foreach ($result as $key=>$value) {
     echo $key . '=' . $value . '<br>';
