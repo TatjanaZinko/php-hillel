016785e5d4ec4e0bb60479087c26006a4ad45bdc
Zinko <ztvproweb@gmail.com>
2021-03-21 13:05:38 +0200

    Lesson 5

--- index.php
+++ index.php
@@ -1,47 +1,115 @@
 <?php
-/*
- * First Task
- */
-function factory(... $functions) {
-    foreach ($functions as $func) {
-        $func();
+//1 Task
+abstract class Animals {
+    abstract protected function eat();
+}
+
+//2 Task
+abstract class Transport {
+    abstract protected function drive();
+}
+
+//3 Task
+class Predator extends Animals {
+    public function eat(){
+        echo "Eat meat" . "<br>";
     }
 }
 
-factory(function() {
-        echo 'test1<br>';
-    },
-    function() {
-        echo 'test2<br>';
-    },
-    function() {
-        echo 'test3<br>';
-    });
-
-/*
- * Second Task
- */
-
-$file = "resource.txt";
-$content = [1, 'key'=>'test2','test3'];
-
-function write_csv(string $file_name, array $text) {
-    $current_file = fopen($file_name, "w");
-    fputcsv ($current_file, $text);
-    fclose($current_file);
+class Herbivorous extends Animals {
+    public function eat(){
+        echo "Eat grass" . "<br>";
+    }
 }
-write_csv($file, $content);
 
-/*
- * Third Task
- */
-function read_csv(string $file_name) {
-    $current_file = fopen($file_name, "r");
-    $data = fgetcsv ($current_file, 1000);
-    fclose($current_file);
-    var_export($data);
-    return $data;
+//4 Task
+class Boat extends Transport {
+    public function drive(){
+        echo "Float on water" . "<br>";
+    }
+}
+
+class Car extends Transport {
+    public function drive(){
+        echo "Drive on road" . "<br>";
+    }
+}
+
+class Truck extends Transport {
+    public function drive(){
+        echo "Carry cargo" . "<br>";
+    }
+}
+
+//5 Task
+
+$lion = new Predator;
+$lion->eat();
+
+$goat = new Herbivorous;
+$goat->eat();
+
+$sailboat = new Boat;
+$sailboat->drive();
+
+$kia = new Car;
+$kia->drive();
+
+$daf= new Truck;
+$daf->drive();
+
+//6 Task
+
+class ArrayHelper {
+
+    static function firstElement(array $array) {
+        return current($array);
+    }
+
+    static function removeZeroElement(array $array): array
+    {
+        return array_filter ($array, function ($var){
+            return ($var !== 0);
+        });
+    }
+}
+
+//7 Task
+
+class StringHelper {
+
+    static function countLetter(string $string):void
+    {
+        $arr = preg_split('//u',$string,-1,PREG_SPLIT_NO_EMPTY);
+        $result = array_count_values($arr);
+        foreach ($result as $key=>$value) {
+            echo $key . '=' . $value . '<br>';
+        }
+    }
 
 }
-read_csv($file);
+
+class UserClass {
+    function cleanArray (array $array): array
+    {
+        return ArrayHelper::removeZeroElement($array);
+    }
+    function returnFirst (array $array)
+    {
+        return ArrayHelper::firstElement($array);
+    }
+    function countStringLetter (string $string)
+    {
+        StringHelper::countLetter($string);
+    }
+}
+$arr = [7, 'test', 5, 0];
+$string = 'агава';
+$obj = new UserClass;
+var_export($obj->cleanArray($arr));
+echo "<br>";
+var_export($obj->returnFirst($arr));
+echo "<br>";
+$obj->countStringLetter($string);
+
 ?>
