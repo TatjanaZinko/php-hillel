8fbe0fb65c06e746567c33bda01db6c813c3f027
Zinko <ztvproweb@gmail.com>
2021-03-22 19:47:10 +0200

    Lessons 6

--- index.php
+++ index.php
@@ -1,115 +1,77 @@
 <?php
-//1 Task
-abstract class Animals {
-    abstract protected function eat();
-}
-
-//2 Task
-abstract class Transport {
+abstract class Transport
+{
     abstract protected function drive();
 }
 
-//3 Task
-class Predator extends Animals {
-    public function eat(){
-        echo "Eat meat" . "<br>";
+interface TransportInterface
+{
+    public function getType():string;
+
+    public function getWheelFormula():string;
+
+    public function getEngine():string;
+
+    public function getTransmission():string;
+}
+
+trait Calculator
+{
+    public function calcEnginePower(int $engineVolume, float $meanEffectivePressure, int $rotationFrequency):float
+    {
+        return $engineVolume * $meanEffectivePressure * $rotationFrequency / 120;
     }
 }
 
-class Herbivorous extends Animals {
-    public function eat(){
-        echo "Eat grass" . "<br>";
-    }
-}
+class Car extends Transport implements TransportInterface
+{
+    use Calculator;
 
-//4 Task
-class Boat extends Transport {
+    protected $type = 'sedan';
+
+    protected $wheelFormula = '4x2';
+
+    protected $engine = 'petrol';
+
+    protected $transmission = 'auto';
+
+    public function getType():string
+    {
+        return $this->type;
+    }
+
+    public function getWheelFormula():string
+    {
+        return $this->wheelFormula;
+    }
+
+    public function getEngine():string
+    {
+        return $this->engine;
+    }
+
+    public function getTransmission():string
+    {
+        return $this->transmission;
+    }
+
     public function drive(){
-        echo "Float on water" . "<br>";
+        echo "Drive on road";
     }
 }
-
-class Car extends Transport {
-    public function drive(){
-        echo "Drive on road" . "<br>";
-    }
-}
-
-class Truck extends Transport {
-    public function drive(){
-        echo "Carry cargo" . "<br>";
-    }
-}
-
-//5 Task
-
-$lion = new Predator;
-$lion->eat();
-
-$goat = new Herbivorous;
-$goat->eat();
-
-$sailboat = new Boat;
-$sailboat->drive();
 
 $kia = new Car;
 $kia->drive();
+echo '<br>';
+var_export($kia->getType());
+echo '<br>';
+var_export($kia->getWheelFormula());
+echo '<br>';
+var_export($kia->getEngine());
+echo '<br>';
+var_export($kia->getTransmission());
+echo '<br>';
+var_export($kia->calcEnginePower(1500, 0.9, 5000));
 
-$daf= new Truck;
-$daf->drive();
-
-//6 Task
-
-class ArrayHelper {
-
-    static function firstElement(array $array) {
-        return current($array);
-    }
-
-    static function removeZeroElement(array $array): array
-    {
-        return array_filter ($array, function ($var){
-            return ($var !== 0);
-        });
-    }
-}
-
-//7 Task
-
-class StringHelper {
-
-    static function countLetter(string $string):void
-    {
-        $arr = preg_split('//u',$string,-1,PREG_SPLIT_NO_EMPTY);
-        $result = array_count_values($arr);
-        foreach ($result as $key=>$value) {
-            echo $key . '=' . $value . '<br>';
-        }
-    }
-
-}
-
-class UserClass {
-    function cleanArray (array $array): array
-    {
-        return ArrayHelper::removeZeroElement($array);
-    }
-    function returnFirst (array $array)
-    {
-        return ArrayHelper::firstElement($array);
-    }
-    function countStringLetter (string $string)
-    {
-        StringHelper::countLetter($string);
-    }
-}
-$arr = [7, 'test', 5, 0];
-$string = 'агава';
-$obj = new UserClass;
-var_export($obj->cleanArray($arr));
-echo "<br>";
-var_export($obj->returnFirst($arr));
-echo "<br>";
-$obj->countStringLetter($string);
 
 ?>
